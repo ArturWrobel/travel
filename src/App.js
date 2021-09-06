4225f1de247ffa6d34f3684871da71dccdcf52d1
ArturWrobel <artur.r.wrobel@gmail.com>
2021-09-06 20:43:37 +0200

    maps

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { CssBaseline, Grid } from '@material-ui/core'
 
+import { getPlacesData } from './api'
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
-import { getPlacesData } from './api'
 
 const App = () => {
     const [places, setPlaces] = useState([])
-    const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 })
-    const [bounds, setBounds] = useState(null)
+    const [coordinates, setCoordinates] = useState({})
+    const [bounds, setBounds] = useState({})
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-            setCoordinates({ lat: latitude, lng: longitude })
-        })
-    }, [])
+            setCoordinates({ lat: latitude, lng: longitude });
+        });
+    }, []);
 
     useEffect(() => {
         getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
-                console.log(data)
                 setPlaces(data)
             })
-    }, [coordinates, bounds])
+    }, [coordinates, bounds]);
 
     return (
         <>
             <CssBaseline />
             <Header />
-            <Grid container spacing={3} style={{ width: '100%' }} >
+            <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List places={places} />
                 </Grid>
@@ -38,6 +37,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places = {places}
                     />
                 </Grid>
             </Grid>
--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,22 @@
-import axios from 'axios'
-
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+import axios from 'axios';
 
 export const getPlacesData = async (sw, ne) => {
     try {
-        const { data: { data } } = await axios.get(URL,
-            {
-                params: {
-                    bl_latitude: sw.lat,
-                    tr_latitude: ne.lat,
-                    bl_longitude: sw.lng,
-                    tr_longitude: ne.lng
-                },
-                headers: {
-                    'x-rapidapi-key': '27debe168dmsh5547f4364d63ceap158e62jsnb92d786b9f2c',
-                    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
-                }
-            })
-        return data
-    } catch (e) {
-        console.log(e)
+        const { data: { data } } = await axios.get('https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary', {
+            params: {
+                bl_latitude: sw.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+                tr_latitude: ne.lat,
+            },
+            headers: {
+                'x-rapidapi-key': '27debe168dmsh5547f4364d63ceap158e62jsnb92d786b9f2c',
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            },
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error);
     }
-}
+};
